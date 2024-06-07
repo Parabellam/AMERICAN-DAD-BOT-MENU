c449775cc8841328c7d9f2fcf38307e3628d0c97
Parabellam <stiven.ruiz.991031@gmail.com>
2024-06-07 08:56:51 -0500

    [SR] Add: Sound alert for actividad sospechosa

--- Components/FindActividadSospechosa.py
+++ Components/FindActividadSospechosa.py
@@ -1,11 +1,21 @@
 import os
 import pyautogui
+import pygame
+import time
 
 ACTIVIDAD_SOSPECHOSA_PATH = "Images/Errors"
 
 actividad_sospechosa_imgs = {
     "img1": os.path.join(ACTIVIDAD_SOSPECHOSA_PATH, "actividad_sospechosa_1.png"),
 }
+
+
+pygame.mixer.init()
+
+
+def play_alert_sound():
+    pygame.mixer.music.load("va_a_jugar_o_que.mp3")
+    pygame.mixer.music.play()
 
 
 def isActividadSospechosa():
@@ -16,5 +26,8 @@ def isActividadSospechosa():
             count += 1
             if location:
                 print("Se ha detectado una actividad sospechosa.")
+                for _ in range(5):
+                    play_alert_sound()
+                    time.sleep(10)
                 return True
     return False
